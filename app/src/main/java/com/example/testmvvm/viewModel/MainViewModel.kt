4c5d82bf23dc327f449b57051327855e38bac0a9
MUradyan <hayk.muradyan@zangi.com>
2021-11-03 11:09:13 +0300

    (HM) is added Volley in MainViewModel.kt

--- app/src/main/java/com/example/testmvvm/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/testmvvm/viewModel/MainViewModel.kt
@@ -3,21 +3,44 @@ package com.example.testmvvm.viewModel
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.testmvvm.model.Car
 import io.realm.Realm
 import io.realm.RealmQuery
 import io.realm.RealmResults
+import org.json.JSONObject
 import java.util.*
 import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
     var realmDB: Realm? = null
+    var requestQueue: RequestQueue? = null
 
     fun configure(context: Context) {
         Realm.init(context)
         realmDB = Realm.getDefaultInstance()
 
+        requestQueue = Volley.newRequestQueue(context)
+        parse()
 //        cars?.value = DataManager.createCarDataSource()
+    }
+
+    fun parse() {
+        val url = "https://swapi.dev/api/starships/12/"
+        val request = JsonObjectRequest(Request.Method.GET, url, null, {
+            //onResponse
+            val jsonObject = it
+            addItem(jsonObject.getString("name"))
+
+        }, {
+            //onError
+            it.printStackTrace()
+        })
+
+        requestQueue?.add(request)
     }
 
     fun addItem(name: String) {
